db812eda652d3fbe330097398da19dee74bc2980
Kevin Bell <bellKevin@pm.me>
2022-09-08 14:50:00 -0600

    Update Souvenirs.java

--- baseballParkSales/src/baseballparksales/Souvenirs.java
+++ baseballParkSales/src/baseballparksales/Souvenirs.java
@@ -24,13 +24,16 @@ public class Souvenirs extends Inventory {
     public double souvenirBulkDiscountCalc(double qty) {
         if (qty > 15) {
             System.out.println("Volume discount rate of 15.00 percent");
-            return unitPrice = (unitPrice * DISCOUNT_RATE_15);
+            //return unitPrice = (unitPrice * DISCOUNT_RATE_15);
+            return 1000;
         } else if (qty > 8 && qty < 16) {
             System.out.println("Volume discount rate of 5.00 percent");
-            return unitPrice = (unitPrice * DISCOUNT_RATE_8);
+            //return unitPrice = (unitPrice * DISCOUNT_RATE_8);
+            return 50;
         } else {
             System.out.println("no discount");
-            return unitPrice;
+            // return unitPrice;
+            return 5000;
         } // if/else if condition
 
 // ((unitPrice*qty)-((unitPrice*qty)*DISCOUNT_RATE_15))+(((unitPrice*qty)-
@@ -51,6 +54,7 @@ public class Souvenirs extends Inventory {
             return total;
         } else {
             System.out.println("Not enough quantity on hand to sell");
+            display();
             return 0;
         }// end if/else condition 
     } // end calcCost method
