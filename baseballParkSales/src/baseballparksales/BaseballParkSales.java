6fca6eae1bb2405e71c30ae3413f9707989d8d42
Kevin Bell <bellKevin@pm.me>
2022-09-07 19:29:40 -0600

    Add files via upload

---
+++ baseballParkSales/src/baseballparksales/BaseballParkSales.java
@@ -0,0 +1,14 @@
+/** ****************************************************************************
+ * BaseballParkSales.java
+ * Kevin Bell
+ *
+ * This driver program simulates sales by vendors in stands at a baseball park.
+ **************************************************************************** */
+package baseballparksales;
+
+public class BaseballParkSales {
+
+    public static void main(String[] args) {
+        System.out.println("Module 2 Baseball Park Sales by Kevin Bell");
+    } // end main method
+} // end BaseballParkSales class
---
+++ baseballParkSales/src/baseballparksales/SouvenirBulkDiscount.java
@@ -0,0 +1,11 @@
+/** ****************************************************************************
+ * SouvenirBulkDiscount.java
+ * Kevin Bell
+ *
+ * This interface simulates bulk discounts on souvenirs
+ **************************************************************************** */
+package baseballparksales;
+
+public interface SouvenirBulkDiscount {
+
+} // end interface SouvenirBulkDiscount
